08e057bb94593bd0d6f91078bffe80d598c3c124
Vitorsantosdomingues <domingues.vitor@escola.pr.gov.br>
2023-08-19 01:42:51 -0300

    Update v1.0
    Está versão possui apenas header e algumas configurações CSS, e com pastas estruturadas

---
+++ src/components/header/index.jsx
@@ -0,0 +1,11 @@
+import styles from './header.module.scss'
+
+export const HeaderComponent = () => {
+    return(
+        <header>
+            <div className={styles.header__logo}>
+
+            </div>
+        </header>
+    )
+}
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,9 @@
+import { HeaderComponent } from "../../components/header"
+
+export const HomePage = () => {
+    return(
+        <>
+            <HeaderComponent />
+        </>
+    )
+}
