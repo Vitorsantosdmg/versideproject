9af6f159a40771e5442d717d20a329ad7c2721a1
Vitorsantosdmg <140419874+Vitorsantosdmg@users.noreply.github.com>
2023-08-24 11:58:44 -0300

    mk 0.09 v1.3

--- src/components/mainHome/index.jsx
+++ src/components/mainHome/index.jsx
@@ -4,7 +4,12 @@ export const MainHome = () => {
     return(
         <main>
             <div className={styles.mainHome__title}>
-                <h1>O melhor lado da sua versão</h1>
+                <h1>A</h1>
+                <h1>melhor</h1>
+                <h1>versão</h1>
+                <h1>de</h1>
+                <h1>você</h1>
+                <h1>mesmo.</h1>
             </div>
             
         </main>
