657d34c7dc95eed900d8dab70a4e46d2d13b0e6f
Vitorsantosdmg <140419874+Vitorsantosdmg@users.noreply.github.com>
2023-09-21 12:26:32 -0300

    updateWeb

--- src/components/sections/aboutHome/ratingSection/index.jsx
+++ src/components/sections/aboutHome/ratingSection/index.jsx
@@ -4,7 +4,6 @@ import styles from './style.module.scss'
 export const RatingSection = () => {
     return(
         <>
-            <h1>Depoimentos</h1>
             <div className={styles.ratingSection}>
                 <RatingCard />
                 <RatingCard />
