d982aac3279bc6f35af2f5255380ecda0bacba3e
VitorSantosDev <123842676+Vitorsantosdomingues@users.noreply.github.com>
2023-09-21 00:56:23 -0300

    terminei o mainHome

--- src/components/sections/mainHome/index.jsx
+++ src/components/sections/mainHome/index.jsx
@@ -9,9 +9,7 @@ export const MainHome = () => {
                 <h1>Conheça a sua melhor versão</h1>
                 <Button2 classIcon="material-symbols-outlined" icon="arrow_downward" content="Confira nossos produtos" />
             </div>
-            <div className={styles.mainHome__person}>
-                
-            </div>
+            <div className={styles.mainHome__person}></div> 
         </main>
     )
 }
