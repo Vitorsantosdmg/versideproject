3cf7c081a2e800aab211297cf28ea4b1bf3264a2
Vitorsantosdmg <140419874+Vitorsantosdmg@users.noreply.github.com>
2023-09-21 12:17:03 -0300

    updateWeb

--- src/components/sections/aboutHome/ratingSection/index.jsx
+++ src/components/sections/aboutHome/ratingSection/index.jsx
@@ -1,9 +1,12 @@
+import { RatingCard } from './ratingCard'
 import styles from './style.module.scss'
 
 export const RatingSection = () => {
     return(
         <div className={styles.ratingSection}>
-
+            <RatingCard />
+            <RatingCard />
+            <RatingCard />
         </div>
     )
 }
---
+++ src/components/sections/aboutHome/ratingSection/ratingCard/index.jsx
@@ -0,0 +1,9 @@
+import styles from './style.module.scss';
+
+export const RatingCard = () => {
+    return(
+        <div className={styles.ratingCard}>
+
+        </div>
+    )
+}
