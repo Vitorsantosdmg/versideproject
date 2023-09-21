ad6d832462a6cefda4c0f075f9b80b1483a7c019
Vitorsantosdmg <140419874+Vitorsantosdmg@users.noreply.github.com>
2023-09-21 12:35:41 -0300

    updateWeb

--- src/components/sections/aboutHome/ratingSection/ratingCard/index.jsx
+++ src/components/sections/aboutHome/ratingSection/ratingCard/index.jsx
@@ -2,8 +2,10 @@ import styles from './style.module.scss';
 
 export const RatingCard = () => {
     return(
-        <div className={styles.ratingCard}>
-
+        <div className={styles.ratingCard__container}>
+            <div className={styles.ratingCard__user}>
+                
+            </div>
         </div>
     )
 }
