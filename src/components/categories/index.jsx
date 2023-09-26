83d38771e1684a1ecb3920f7840c5b4883d3a5d7
VitorSantosDev <123842676+Vitorsantosdomingues@users.noreply.github.com>
2023-09-26 14:07:37 -0300

    Adicionando gêneros a barra de categorias

--- src/components/categories/index.jsx
+++ src/components/categories/index.jsx
@@ -1,16 +1,9 @@
-
-import { SelectGender } from './gender';
 import styles from './style.module.scss';
 
 export const CategoriesComponent = () => {
     return(
         <div className={styles.categories}>
-            <div className={styles.categories__container}>
-               <div className={styles.categories__select}>
-                    <SelectGender />
-
-               </div>
-            </div>
+            
         </div>
     )
 }
